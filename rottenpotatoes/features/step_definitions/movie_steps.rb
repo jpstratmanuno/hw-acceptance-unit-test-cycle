93d241d31dca946b39a8c308b4d5346a2c933c48
Jason Stratman <jpstratman@unomaha.edu>
2017-07-04 22:33:23 +0000

    Changed database settings

---
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,8 @@
+Given(/^the following movies exist:$/) do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the director of "([^"]*)" should be "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
