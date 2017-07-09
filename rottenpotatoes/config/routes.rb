b9f5c17fc75e45b33b336231d0b52194272c3c12
Jason Stratman <jpstratman@unomaha.edu>
2017-07-09 18:13:43 +0000

    Changed routes

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,7 +1,5 @@
 Rottenpotatoes::Application.routes.draw do
-  resources :movies do
-    get 'directorview'
-  end
+  resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
 end
