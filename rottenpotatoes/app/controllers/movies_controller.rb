e560073156063a8c76ee7115d84a542107322d47
Jason Stratman <jpstratman@unomaha.edu>
2017-07-09 15:15:48 +0000

    Corrected linked directory in show

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -10,9 +10,10 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
   
-  def same_director
+  def director
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/director.<extension> by default
   end
 
   def index
